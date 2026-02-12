28e1a3c0c133fd348c7f5274f2d87b19a73ad7ee
marcyn <suchovsky.martin12@gmail.com>
2026-02-12 02:12:03 +0100

    added pink_garnet item added raw_pink_garnet item

--- src/main/java/net/marcynos/tutorialmod/TutorialMod.java
+++ src/main/java/net/marcynos/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.marcynos.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.marcynos.tutorialmod.block.ModBlocks;
+import net.marcynos.tutorialmod.item.ModItemGroups;
 import net.marcynos.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
